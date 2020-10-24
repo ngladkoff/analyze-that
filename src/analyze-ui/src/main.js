cc2e51fc7da0ae2f4e85c6c64351eec5eec895b0
Nicolas Gladkoff <ngladkoff@users.noreply.github.com>
2020-10-23 22:06:58 -0300

    Agregar CoreUI

--- src/analyze-ui/src/main.js
+++ src/analyze-ui/src/main.js
@@ -1,13 +1,27 @@
+import 'core-js/stable'
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import CoreuiVue from '@coreui/vue'
+import { iconsSet as icons } from './assets/icons/icons.js'
+import store from './store'
 import Amplify from 'aws-amplify';
 import '@aws-amplify/ui-vue';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
-Vue.config.productionTip = false
+Vue.config.performance = true
+Vue.use(CoreuiVue)
+Vue.prototype.$log = console.log.bind(console)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  icons,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
